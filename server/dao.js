f758ccde4e15d3ff1566c8b3bedabb7da5a28b80
eugene butovka <eugene.butovka@gmail.com>
2024-06-28 14:22:16 +0300

    upd

--- server/dao.js
+++ server/dao.js
@@ -1,26 +1,30 @@
 'use strict'
 
-const sqlite = require('sqlite3')
+const pg = require('pg')
+const { Client } = pg
+const db = new Client()
+db.connect()
+//const sqlite = require('sqlite3')
 const bcrypt = require('bcrypt')
 
 // open the database
-const db = new sqlite.Database('surveys.db', (err) => {
-  if (err) {
-    throw err
-  }
-})
+// const db = new sqlite.Database('surveys.db', (err) => {
+//   if (err) {
+//     throw err
+//   }
+// })
 
 exports.getSurvey = (idSurvey) => {
-  const sql_query = 'SELECT * FROM Survey where id = ?'
+  const sql_query = 'SELECT * FROM "Survey" where id = $1'
 
   return new Promise((resolve, reject) => {
-    db.get(sql_query, [idSurvey], (err, row) => {
+    db.query(sql_query, [idSurvey], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
-        reject({error: 'Survey not found!'})
+        reject({error: '"Survey" not found!'})
       } else {
-        resolve(row)
+        resolve(row.rows[0])
       }
     })
   })
@@ -28,66 +32,66 @@ exports.getSurvey = (idSurvey) => {
 
 exports.getFirstSurveyByTitle = (title) => {
   const sql_query = `SELECT *
-                     FROM Survey
-                     where title = ?
+                     FROM "Survey"
+                     where title = $1
                      ORDER BY id desc LIMIT 1`
 
   return new Promise((resolve, reject) => {
-    db.get(sql_query, [title], (err, row) => {
+    db.query(sql_query, [title], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
-        reject({error: 'Survey not found!'})
+        reject({error: '"Survey" not found!'})
       } else {
-        resolve(row)
+        resolve(row.rows[0])
       }
     })
   })
 }
 
 exports.getCompletedSurvey = (idCS) => {
-  const sql_query = 'SELECT * FROM CompletedSurvey where id = ?'
+  const sql_query = 'SELECT * FROM "CompletedSurvey" where id = $1'
 
   return new Promise((resolve, reject) => {
-    db.get(sql_query, [idCS], (err, row) => {
+    db.query(sql_query, [idCS], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
         reject({error: 'Results not found!'})
       } else {
-        resolve(row)
+        resolve(row.rows[0])
       }
     })
   })
 }
 
 exports.getIdCompletedFirstSurvey = (idSurvey) => {
-  const sql_query = 'SELECT min(idSurvey) as first FROM CompletedSurvey where idSurvey=?'
+  const sql_query = `SELECT min("idSurvey") as first FROM "CompletedSurvey" where "idSurvey"=$1`
 
   return new Promise((resolve, reject) => {
-    db.get(sql_query, [idSurvey], (err, row) => {
+    db.query(sql_query, [idSurvey], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
-        reject({error: 'Survey not found!'})
+        reject({error: '"Survey" not found!'})
       } else {
-        resolve(row)
+        resolve(row.rows[0])
       }
     })
   })
 }
 
 exports.getIdCompletedNextSurvey = (idCS, idSurvey) => {
-  const sql_query = 'SELECT min(id) as next FROM CompletedSurvey where id>? and idSurvey=?'
+  const sql_query = `SELECT min(id) as next FROM "CompletedSurvey" where id>$1 and "idSurvey"=$2`
 
   return new Promise((resolve, reject) => {
-    db.get(sql_query, [idCS, idSurvey], (err, row) => {
+    db.query(sql_query, [idCS, idSurvey], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
-        reject({error: 'Survey not found!'})
+        reject({error: '"Survey" not found!'})
       } else {
-        resolve(row)
+        resolve(row.rows[0])
       }
     })
   })
@@ -95,95 +99,81 @@ exports.getIdCompletedNextSurvey = (idCS, idSurvey) => {
 
 const getIdCompletedSurveyForUsername = (idSurvey, username) => {
   const sql_query = `SELECT *
-                     FROM CompletedSurvey
-                     where idSurvey = ?
-                       and username = ?
+                     FROM "CompletedSurvey"
+                     where "idSurvey" = $1
+                       and username = $2
                      ORDER BY id desc LIMIT 1`
 
   return new Promise((resolve, reject) => {
-    db.get(sql_query, [idSurvey, username], (err, row) => {
+    db.query(sql_query, [idSurvey, username], (err, row) => {
       if (err) {
         reject(err)
       } else {
-        resolve(row)
+        resolve(row.rows[0])
       }
     })
   })
 }
 
 exports.getAllCompletedSurveysWithAnswersForUsername = (username) => {
-  const sql_query = `SELECT DISTINCT cs.idSurvey as surveyId, Q.id as questionId, UCA.idAnswer as answer
-                     FROM CompletedSurvey CS
-                              INNER JOIN main.Question Q on cs.idSurvey = Q.idSurvey
-                              INNER JOIN main.Answer A on Q.id = A.idQuestion
-                              INNER JOIN main.UserClosedAnswer UCA on A.id = UCA.idAnswer
-                     where username = ?`
+  const sql_query = `SELECT DISTINCT cs."idSurvey" as "surveyId", Q.id as "questionId", UCA."idAnswer" as "answer"
+                     FROM "CompletedSurvey" CS
+                              INNER JOIN "Question" Q on cs."idSurvey" = Q."idSurvey"
+                              INNER JOIN "Answer" A on Q.id = A."idQuestion"
+                              INNER JOIN "UserClosedAnswer" UCA on A.id = UCA."idAnswer"
+                     where username = $1`
 
   return new Promise((resolve, reject) => {
-    db.all(sql_query, [username], (err, row) => {
+    db.query(sql_query, [username], (err, row) => {
       if (err) {
         reject(err)
       } else {
-        resolve(row)
+        resolve(row.rows)
       }
     })
   })
 }
 
 exports.getQuestions = (idSurvey) => {
-  const sql_query = 'SELECT * FROM Question where idSurvey = ?'
+  const sql_query = 'SELECT * FROM "Question" where "idSurvey" = $1'
 
   return new Promise((resolve, reject) => {
-    db.all(sql_query, [idSurvey], (err, rows) => {
+    db.query(sql_query, [idSurvey], (err, rows) => {
       if (err) {
         reject(err)
       } else {
-        resolve(rows)
+        resolve(rows.rows)
       }
     })
   })
 }
 
 exports.getAnswers = (idQuestion) => {
-  const sql_query = 'SELECT * FROM Answer where idQuestion = ?'
+  const sql_query = 'SELECT * FROM "Answer" where "idQuestion" = $1'
 
   return new Promise((resolve, reject) => {
-    db.all(sql_query, [idQuestion], (err, rows) => {
+    db.query(sql_query, [idQuestion], (err, rows) => {
       if (err) {
         reject(err)
       } else {
-        resolve(rows)
-      }
-    })
-  })
-}
-
-exports.getSurveys = () => {
-  const sql_getSurveys = 'SELECT * FROM survey'
-
-  return new Promise((resolve, reject) => {
-    db.all(sql_getSurveys, [], (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
+        resolve(rows.rows)
       }
     })
   })
 }
 
 exports.getUserClosedAnswers = (idQuestion, idCS) => {
-  const sql_query = 'select idAnswer ' +
-      'from UserClosedAnswer, Answer ' +
-      'where Answer.idQuestion = ? and idCompletedSurvey = ? ' +
-      'and Answer.id = UserClosedAnswer.idAnswer'
+  const sql_query = 'select "idAnswer" ' +
+      'from "UserClosedAnswer", "Answer" ' +
+      'where "Answer"."idQuestion" = $1 and "idCompletedSurvey" = $2 ' +
+      'and "Answer".id = "UserClosedAnswer"."idAnswer"'
 
   return new Promise((resolve, reject) => {
-    db.all(sql_query, [idQuestion, idCS], (err, rows) => {
+    db.query(sql_query, [idQuestion, idCS], (err, rows) => {
       if (err) {
         reject(err);
       } else {
-        resolve(rows);
+        resolve(rows.rows);
       }
     })
   })
@@ -191,79 +181,79 @@ exports.getUserClosedAnswers = (idQuestion, idCS) => {
 
 exports.getUserOpenAnswers = (idQuestion, idCS) => {
   const sql_query = 'SELECT text ' +
-      'from UserOpenAnswer ' +
-      'where idQuestion = ? and idCompletedSurvey = ?'
+      'from "UserOpenAnswer" ' +
+      'where "idQuestion" = $1 and "idCompletedSurvey" = $2'
 
   return new Promise((resolve, reject) => {
-    db.get(sql_query, [idQuestion, idCS], (err, row) => {
+    db.query(sql_query, [idQuestion, idCS], (err, row) => {
       if (err) {
         reject(err)
       } else {
-        resolve(row)
+        resolve(row.rows[0])
       }
     })
   })
 }
 
 exports.getSurveys = () => {
-  const sql_getSurveys = 'SELECT * FROM survey'
+  const sql_getSurveys = 'SELECT * FROM "Survey"'
 
   return new Promise((resolve, reject) => {
-    db.all(sql_getSurveys, [], (err, rows) => {
+    db.query(sql_getSurveys, [], (err, rows) => {
       if (err) {
         reject(err);
       } else {
-        resolve(rows);
+        resolve(rows.rows);
       }
     })
   })
 }
 
 exports.getAdminSurveys = (idAdmin) => {
-  const sql_getSurveys = 'select min(CompletedSurvey.id) as next, Survey.id, Survey.title, count(CompletedSurvey.id) as count '
+  const sql_getSurveys = 'select min("CompletedSurvey".id) as next, "Survey".id, "Survey".title, count("CompletedSurvey".id) as count '
       +
-      'from Survey left join CompletedSurvey on Survey.id = CompletedSurvey.idSurvey '
+      'from "Survey" left join "CompletedSurvey" on "Survey".id = "CompletedSurvey"."idSurvey" '
       +
-      'where idAdmin = ? ' +
-      'group by (Survey.id)' +
-      'order by Survey.id desc'
+      'where idAdmin = $1 ' +
+      'group by ("Survey".id)' +
+      'order by "Survey".id desc'
 
   return new Promise((resolve, reject) => {
-    db.all(sql_getSurveys, [idAdmin], (err, rows) => {
+    db.query(sql_getSurveys, [idAdmin], (err, rows) => {
       if (err) {
         reject(err)
       } else {
-        resolve(rows)
+        resolve(rows.rows)
       }
     })
   })
 }
 
 const getIdSurvey = () => {
-  const sql_getId = "select max(id) as num from Survey"
+  const sql_getId = `select max(id) as num from "Survey"`
   return new Promise((resolve, reject) => {
-    db.get(sql_getId, [], (err, row) => {
+    db.query(sql_getId, [], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
         reject(err)
       } else {
-        resolve(row.num)
+        resolve(row.rows[0].num)
       }
     })
   })
 }
 
 const getIdQuestion = () => {
-  const sql_getId = "select max(id) as num from Question"
+  const sql_getId = `select max(id) as num from "Question"`
   return new Promise((resolve, reject) => {
-    db.get(sql_getId, [], (err, row) => {
+    db.query(sql_getId, [], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
         reject(err)
       } else {
-        resolve(row.num)
+        resolve(row.rows[0].num)
       }
     })
   })
@@ -271,10 +261,10 @@ const getIdQuestion = () => {
 
 exports.insertSurvey = async (survey, idAdmin) => {
   const surveyId = await getIdSurvey() + 1
-  const sql_query = "INSERT INTO Survey(id, title, idAdmin) VALUES(?, ?, ?)"
+  const sql_query = `INSERT INTO "Survey"(id, title, "idAdmin") VALUES($1, $2, $3)`
 
   return new Promise((resolve, reject) => {
-    db.run(sql_query, [surveyId, survey.title, idAdmin], (error) => {
+    db.query(sql_query, [surveyId, survey.title, idAdmin], (error) => {
       if (error) {
         reject(error)
       } else {
@@ -287,9 +277,9 @@ exports.insertSurvey = async (survey, idAdmin) => {
 exports.insertQuestion = async (idSurvey, question) => {
   const idQuestion = await getIdQuestion() + 1
 
-  const sql_query = "INSERT INTO Question(id, idSurvey, text, type, min, max) VALUES (?, ?, ?, ?, ?, ?)"
+  const sql_query = `INSERT INTO "Question"(id, "idSurvey", text, type, min, max) VALUES ($1, $2, $3, $1, $2, $3)`
   return new Promise((resolve, reject) => {
-    db.run(sql_query,
+    db.query(sql_query,
         [idQuestion, idSurvey, question.text, question.type, question.min,
           question.max], (error) => {
           if (error) {
@@ -302,10 +292,10 @@ exports.insertQuestion = async (idSurvey, question) => {
 }
 
 exports.insertAnswer = async (idQuestion, answer) => {
-  const sql_query = "INSERT INTO Answer(idQuestion, text) VALUES (?, ?)"
+  const sql_query = `INSERT INTO "Answer"("idQuestion", text) VALUES ($1, $2)`
 
   return new Promise((resolve, reject) => {
-    db.run(sql_query, [idQuestion, answer.text], (error) => {
+    db.query(sql_query, [idQuestion, answer.text], (error) => {
       if (error) {
         reject(error);
       } else {
@@ -316,15 +306,15 @@ exports.insertAnswer = async (idQuestion, answer) => {
 }
 
 const getIdCompletedSurvey = () => {
-  const sql_getId = "select max(id) as num from CompletedSurvey"
+  const sql_getId = `select max(id) as num from "CompletedSurvey"`
   return new Promise((resolve, reject) => {
-    db.get(sql_getId, [], (err, row) => {
+    db.query(sql_getId, [], (err, row) => {
       if (err) {
         reject(err)
       } else if (row === undefined) {
         reject("Results not found!")
       } else {
-        resolve(row.num)
+        resolve(row.rows[0].num)
       }
     })
   })
@@ -332,17 +322,14 @@ const getIdCompletedSurvey = () => {
 
 exports.insertOrReplaceCompletedSurvey = async (idSurvey, username) => {
   const compSurvey = await getIdCompletedSurveyForUsername(idSurvey, username)
-  let compSurveyId
-  let sql_query
-  if (compSurvey != null) {
-    compSurveyId = compSurvey.id
-    sql_query = "REPLACE INTO CompletedSurvey(id, idSurvey, username) VALUES (?, ?, ?)"
-  } else {
-    compSurveyId = await getIdCompletedSurvey() + 1
-    sql_query = "INSERT INTO CompletedSurvey(id, idSurvey, username) VALUES (?, ?, ?)"
-  }
+  const compSurveyId = compSurvey?.id || await getIdCompletedSurvey() + 1
+  const sql_query = `INSERT INTO "CompletedSurvey"(id, "idSurvey", username) VALUES ($1, $2, $3)
+        ON CONFLICT (id) DO UPDATE
+                                SET "idSurvey" = excluded."idSurvey",
+                                                 username = excluded.username`
+
   return new Promise((resolve, reject) => {
-    db.run(sql_query, [compSurveyId, idSurvey, username], (error) => {
+    db.query(sql_query, [compSurveyId, idSurvey, username], (error) => {
       if (error) {
         reject(error);
       } else {
@@ -353,9 +340,9 @@ exports.insertOrReplaceCompletedSurvey = async (idSurvey, username) => {
 }
 
 exports.insertUserClosedAnswer = async (idAnswer, idCS) => {
-  const sql_query = "INSERT INTO UserClosedAnswer(idAnswer, idCompletedSurvey) VALUES (?, ?)"
+  const sql_query = `INSERT INTO "UserClosedAnswer"("idAnswer", "idCompletedSurvey") VALUES ($1, $2)`
   return new Promise((resolve, reject) => {
-    db.run(sql_query, [idAnswer, idCS], (error) => {
+    db.query(sql_query, [idAnswer, idCS], (error) => {
       if (error) {
         reject(error);
       } else {
@@ -366,9 +353,9 @@ exports.insertUserClosedAnswer = async (idAnswer, idCS) => {
 }
 
 exports.deleteUserClosedAnswers = async (idCS) => {
-  const sql_query = "DELETE FROM UserClosedAnswer where idCompletedSurvey=?"
+  const sql_query = `DELETE FROM "UserClosedAnswer" where "idCompletedSurvey"=$1`
   return new Promise((resolve, reject) => {
-    db.run(sql_query, [idCS], (error) => {
+    db.query(sql_query, [idCS], (error) => {
       if (error) {
         reject(error);
       } else {
@@ -379,9 +366,9 @@ exports.deleteUserClosedAnswers = async (idCS) => {
 }
 
 exports.insertUserOpenAnswer = async (idCS, idQuestion, text) => {
-  const sql_query = "INSERT INTO UserOpenAnswer(idCompletedSurvey, idQuestion, text) VALUES (?, ?, ?)"
+  const sql_query = `INSERT INTO "UserOpenAnswer"("idCompletedSurvey", "idQuestion", text) VALUES ($1, $2, $3)`
   return new Promise((resolve, reject) => {
-    db.run(sql_query, [idCS, idQuestion, text], (error) => {
+    db.query(sql_query, [idCS, idQuestion, text], (error) => {
       if (error) {
         reject(error);
       } else {
@@ -394,8 +381,8 @@ exports.insertUserOpenAnswer = async (idCS, idQuestion, text) => {
 //user validation
 exports.getUser = (username, password) => {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT * FROM Admin WHERE username = ?';
-    db.get(sql, [username], (err, row) => {
+    const sql = 'SELECT * FROM "Admin" WHERE username = $1';
+    db.query(sql, [username], (err, row) => {
       if (err) {
         reject(err);
       }// DB error
@@ -418,8 +405,8 @@ exports.getUser = (username, password) => {
 
 exports.getUserById = (id) => {
   return new Promise((resolve, reject) => {
-    const sql = 'select * from Admin where id = ?';
-    db.get(sql, [id], (err, row) => {
+    const sql = 'select * from "Admin" where id = $1';
+    db.query(sql, [id], (err, row) => {
       if (err) {
         reject(err);
       }// DB error
